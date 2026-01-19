6a99710b7d160c5210eee73b7e956611dd12f48e
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-19 15:56:04 +0200

    Update StatisticsScreen.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsScreen.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.architecture.blueprints.todoapp.statistics
+package com.example.messages
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
