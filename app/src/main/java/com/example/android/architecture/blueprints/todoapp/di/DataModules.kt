93b7b44630e297c6a10febad7a19c6d08e2afb03
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-19 16:01:57 +0200

    Update DataModules.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/di/DataModules.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/di/DataModules.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.architecture.blueprints.todoapp.di
+package com.example.messages
 
 import android.content.Context
 import androidx.room.Room
