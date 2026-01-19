1ae924075209c407822c3d4cfc65cfdc1e8c9568
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-19 15:50:58 +0200

    Update TasksScreen.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksScreen.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksScreen.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.architecture.blueprints.todoapp.tasks
+package com.example.messages
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
