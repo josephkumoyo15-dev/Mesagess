be28351d903842b6174e3d562a38dce6e6f600b9
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-19 15:50:27 +0200

    Update TasksFilterType.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFilterType.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFilterType.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.architecture.blueprints.todoapp.tasks
+package com.example.messages
 
 /**
  * Used with the filter spinner in the tasks list.
