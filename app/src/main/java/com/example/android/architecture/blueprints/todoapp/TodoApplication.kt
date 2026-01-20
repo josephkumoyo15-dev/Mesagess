a1e71a47f0f2ec142c5cbfcd1e24cf6014f0b6a1
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-20 09:41:52 +0200

    Update TodoApplication.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
@@ -2,16 +2,6 @@ package com.example.messages
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
-import timber.log.Timber.DebugTree
 
 @HiltAndroidApp
-class TodoApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(DebugTree())
-        }
-    }
-}
+class TodoApplication : Application()
