1629838ed857ccccfa023189ba3537aaa8bc71f2
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-20 19:35:59 +0200

    Update TodoApplication.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
@@ -4,10 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TodoApplication : Application() {
-    // You can add any global app initialization here if needed
-    override fun onCreate() {
-        super.onCreate()
-        // Example: Initialize logging, analytics, or other libraries
-    }
-}
+class TodoApplication : Application()
