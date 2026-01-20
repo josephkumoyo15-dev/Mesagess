6c942f37c6ca6ba1d010e2f28b0f66c91fd3a8ad
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-20 09:47:34 +0200

    Update TodoActivity.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
@@ -3,18 +3,13 @@ package com.example.messages
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.messages.ui.theme.TodoTheme
-import dagger.hilt.android.AndroidEntryPoint
+import com.example.messages.ui.ChatScreen
 
-@AndroidEntryPoint
 class TodoActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TodoTheme {
-                TodoNavGraph()
-            }
+            ChatScreen()
         }
     }
 }
