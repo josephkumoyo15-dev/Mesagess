eddcf8ea9d170aa73901d2685fbd097eaa103f34
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-20 08:28:52 +0200

    Update TodoApplication.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoApplication.kt
@@ -5,15 +5,11 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
-/**
- * Application class that sets up Timber in DEBUG builds.
- */
 @HiltAndroidApp
 class TodoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        // Initialize Timber for logging
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
         }
