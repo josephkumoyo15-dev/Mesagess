d29db4f6891d5f76146217771b2a75478eda4909
josephkumoyo15-dev <josephkumoyo15@gmail.com>
2026-01-20 04:11:29 +0200

    Update TodoActivity.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.messages
 
 import android.os.Bundle
@@ -22,9 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 
-/**
- * Main activity for the todoapp
- */
 @AndroidEntryPoint
 class TodoActivity : ComponentActivity() {
 
@@ -33,7 +14,7 @@ class TodoActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoTheme {
-                ChatScreen()
+                ChatScreen() // Make sure ChatScreen exists in the same package
             }
         }
     }
